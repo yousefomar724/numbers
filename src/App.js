a2fe83931ce2789811f1aa7608322972c21fa7d5
Yousef Omar <yousefomar724@gmail.com>
2022-12-08 13:59:09 +0200

    policy & terms pages

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import CreateAd from "./pages/CreateAd"
 import Profile from "./pages/Profile"
+import PrivacyAndPolicy from "./pages/PrivacyAndPolicy"
+import TermsAndConditions from "./pages/TermsAndConditions"
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route element={<Home />} path="/" />
-          <Route element={<CreateAd />} path="/create-ad" />
-          <Route element={<Profile />} path="/profile/:id" />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<CreateAd />} path="/create-ad" />
+        <Route element={<Profile />} path="/profile/:id" />
+        <Route element={<PrivacyAndPolicy />} path="/privacy-policy" />
+        <Route element={<TermsAndConditions />} path="/terms-conditions" />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 const Footer = () => {
   return (
     <div className="flex items-center justify-center gap-4 lg:gap-8 py-3 w-full text-center bg-main text-white text-sm font-cairo">
-      <a href="">سياسة الخصوصية</a>
-      <a href="">اتفاقية الاستخدام</a>
+      <Link to="/privacy-policy">سياسة الخصوصية</Link>
+      <Link to="/terms-conditions">اتفاقية الاستخدام</Link>
       <a href="">اتصل بنا</a>
     </div>
   )
